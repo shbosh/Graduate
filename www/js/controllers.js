fd801eca8aeb92ec022ce61c324e3d301088c8ca
Benjamin <ongshunhong@gmail.com>
2015-12-14 19:19:09 +0800

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('app.controllers', [])
+
+.controller('requirementsCtrl', function($scope) {
+
+})
+
+.controller('modulesCtrl', function($scope) {
+
+})
+
+.controller('profileCtrl', function($scope) {
+
+})
+
+.controller('modulestakenCtrl', function($scope){
+
+})
